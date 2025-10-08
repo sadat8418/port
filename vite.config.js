84865410dd02b9e7c90b32292ee47d08a9ec1ddf
sadat8418 <sadat.arefin98@gmail.com>
2025-10-08 15:14:28 +0600

    structured

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
   root: "src",
- build: {
+  build: {
     outDir: "../dist", // build outside of src
     rollupOptions: {
       input: {
