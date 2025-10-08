67158b1895986274789ded81ea8d34ed10680b92
sadat8418 <sadat.arefin98@gmail.com>
2025-10-08 15:58:00 +0600

    structured

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  root: "src",
+  root: ".",
   build: {
-    outDir: "../dist", // build outside of src
+    outDir: "dist", // build outside of src
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: resolve(__dirname, "index.html"),
         page2: resolve(__dirname, "src/index2.html"),
         page3: resolve(__dirname, "src/index3.html"),
         page4: resolve(__dirname, "src/index4.html"),
